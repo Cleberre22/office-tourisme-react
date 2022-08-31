13cf8fc60eab0c10aad4d55af4afb300d699c328
Cleberre22 <c.leberre@hotmail.fr>
2022-08-31 16:58:21 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Menu from "../components/Menu";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
+import axios from "axios";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/restaurant.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
@@ -14,12 +15,16 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow,
 });
 
+
+
+
+
+
+
 const Home = () => {
-
+  const [places, setPlaces] = useState([]);
   return (
-
     <div>
-
       <Menu />
 
       <section className="placeMap ">
@@ -33,14 +38,24 @@ const Home = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[47.27227057515833, -2.205370638084083]}>
+{places.map(place => (
+          <Marker
+            key={place.id}
+            position={[
+              place.latitudePlace,
+              place.longitudePlace,
+            ]}
+          >
+            <Popup>{place.namePlace}</Popup>
+          </Marker>
+           ))}
+          {/* <Marker position={[47.27227057515833, -2.205370638084083]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-          </Marker>
+          </Marker> */}
         </MapContainer>
       </section>
-      
     </div>
   );
 };
